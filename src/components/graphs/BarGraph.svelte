a88a7ce58c5d1137b85e47b31ddc958e3c17436f
jasonlin45 <jasonlin45@outlook.com>
2022-02-17 19:44:52 -0500

    Group charts

---
+++ src/components/graphs/BarGraph.svelte
@@ -0,0 +1,36 @@
+
+<script>
+    import Bar from './Bar.svelte';
+    export let min;
+    export let max;
+    export let data;
+    export let suffix;
+
+    // convert values to pct
+    // [min, max] -> [0, 100]
+    // min = 0;
+    // max = 100;
+    const interval = Math.round(100/(max-min));
+    function map_to_pct(data_point) {
+        return Math.round(interval * data_point);
+    }
+
+</script>
+
+
+<div class="container w-full mx-auto p-2 md:p-0 space-y-8 border-r-2">
+    <div class="w-full mx-auto flex flex-wrap gap-y-8">
+        {#each data as d}
+            <div class="flex basis-full">
+                <div class="basis-1/4 md:basis-3/12 lg:basis-2/12 flex">
+                    <div class="my-auto font-medium text-sm md:text-lg text-right w-full pr-4">
+                        {d.name}
+                    </div>
+                </div>
+                <div class="basis-3/4 md:basis-9/12 lg:basis-10/12">
+                    <Bar width="{map_to_pct(d.num)}">{map_to_pct(d.num)}{suffix}</Bar>
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
